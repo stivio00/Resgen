3a029c61da14ba2300cf7df998448baf8edc704a
Stephen Krol <krol.stephen@gmail.com>
2022-03-15 00:59:50 +0100

    update options class

---
+++ Resgen.lib/Options.cs
@@ -0,0 +1,19 @@
+﻿namespace Resgen.lib;
+public class Options
+{
+    public string InputFile { get; set; }
+    public string OutputDirectory { get; set; }
+    public List<string> OutputFiles { get; set; }
+
+    public Options()
+    {
+        InputFile = "";
+        OutputFiles = new ();
+        OutputDirectory = "";
+    }
+
+    public override string ToString()
+    {
+        return $"files={InputFile}; output={OutputDirectory}; files={string.Join(", ", OutputFiles)}";
+    }
+}
